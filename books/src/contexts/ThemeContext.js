cd7d4ff2137b90aff6d1f128c0c3ec1821f3fb2a
Şeymanur Çürükcü <seymanurcurukcu@gmail.com>
2022-06-12 13:12:14 +0300

    React_KipatListesi
    React ile kitap listesi uygulaması

---
+++ books/src/contexts/ThemeContext.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const ThemeContext = React.createContext();
+    class ThemeContextProvider extends React.Component{
+        state={
+          isDarkTheme: true,
+          dark:{bg: '#222529',txt:'#D65F5f', hover: 'rbga(231, 76, 60, 0.9)'},
+          light:{bg: '#F8F9FA',txt:'#222529', hover: 'rbga(254, 209, 54, 0.9)'},
+        }
+        changeTheme=()=>{
+            this.setState({isDarkTheme:!this.state.isDarkTheme});
+        }
+        render(){
+            return(
+                <ThemeContext.Provider value={{...this.state,changeTheme:this.changeTheme}}>
+                    {this.props.children}
+                </ThemeContext.Provider>
+            )
+        }
+    }
+
+    export default ThemeContextProvider;
+
+
+
